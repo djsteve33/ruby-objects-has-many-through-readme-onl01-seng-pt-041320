1cd3409e0216c0bbcdf62050e700606f20aa6f36
Steve Womble <swomble@aol.com>
2020-05-17 22:08:49 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -3,8 +3,15 @@ attr_accessor :waiter, :customer, :total, :tip
 
 @@all = []
 
-def initialize(waiter, customer, total, tip=0)
-  @waiter = waiter
-  @customer = customer
-  @total = tot
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end
 end
